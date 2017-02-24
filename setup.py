61e3ae9fadea6da78b780180b2ffc11a8c2861b4
Giulio Rossetti <giulio.rossetti@gmail.com>
2017-02-24 14:48:09 +0100

    bugfix

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+# from codecs import open
+# from os import path
+
+__author__ = 'rossetti'
+__license__ = "GPL"
+__email__ = "giulio.rossetti@gmail.com"
+
+# here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+# with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+setup(name='ndlib',
+      version='1.0',
+      license='GNU General Public License v3 or later (GPLv3+)',
+      description='Network Diffusion Library',
+      url='https://github.com/GiulioRossetti/ndlib',
+      author='Giulio Rossetti',
+      author_email='giulio.rossetti@gmail.com',
+      classifiers=[
+          # How mature is this project? Common values are
+          #   3 - Alpha
+          #   4 - Beta
+          #   5 - Production/Stable
+          'Development Status :: 5 - Production/Stable',
+
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+          "Operating System :: OS Independent",
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7'
+      ],
+      keywords='epidemics opinion-dynamics simulator complex-networks',
+      install_requires=['numpy', 'networkx', 'scipy'],
+      packages=find_packages(),
+
+      )
