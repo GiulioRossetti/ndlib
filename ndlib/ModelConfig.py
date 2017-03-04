d7989bd468e2f4e97dbdd14ab2e77a9c35bbe681
Giulio Rossetti <giulio.rossetti@gmail.com>
2017-03-04 10:27:51 +0100

    Model Configuration

---
+++ ndlib/ModelConfig.py
@@ -0,0 +1,39 @@
+__author__ = 'Giulio Rossetti'
+__license__ = "GPL"
+__email__ = "giulio.rossetti@gmail.com"
+
+
+class Configuration(object):
+
+    def __init__(self):
+        self.config = {
+            'nodes': {},
+            'edges': {},
+            'model': {}
+        }
+
+    def get_configuration(self):
+        return self.config
+
+    def add_model_parameter(self, param_name, param_value):
+        self.config['model'][param_name] = param_value
+
+    def add_node_configuration(self, param_name, node_id, param_value):
+        if param_name not in self.config['nodes']:
+            self.config['nodes'][param_name] = {node_id: param_value}
+        else:
+            self.config['nodes'][param_name][node_id] = param_value
+
+    def add_node_set_configuration(self, param_name, node_to_value):
+        for nid, val in node_to_value.iteritems():
+            self.add_node_configuration(param_name, nid, val)
+
+    def add_edge_configuration(self, param_name, edge, param_value):
+        if param_name not in self.config['edges']:
+            self.config['edges'][param_name] = {edge: param_value}
+        else:
+            self.config['edges'][param_name][edge] = param_value
+
+    def add_edge_set_configuration(self, param_name, edge_to_value):
+        for edge, val in edge_to_value.iteritems():
+            self.add_node_configuration(param_name, edge, val)
