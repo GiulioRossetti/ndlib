9b12f07937dc6a53c6e34d4ae6cc307e1a453497
Giulio Rossetti <giulio.rossetti@gmail.com>
2017-05-28 15:45:39 +0200

    documentation

--- docs/conf.py
+++ docs/conf.py
@@ -115,7 +115,6 @@ pygments_style = 'sphinx'
 # If true, keep warnings as "system message" paragraphs in the built documents.
 #keep_warnings = False
 
-# If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
 
