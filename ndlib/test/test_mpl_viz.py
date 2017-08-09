eb36f799b237bd52ea7960b82c0930499b67f9b2
Giulio Rossetti <giulio.rossetti@gmail.com>
2017-08-09 17:05:48 +0200

    added tests

---
+++ ndlib/test/test_mpl_viz.py
@@ -0,0 +1,115 @@
+from __future__ import absolute_import
+
+import unittest
+import os
+
+import networkx as nx
+from ndlib.viz.mpl.DiffusionPrevalence import DiffusionPrevalence
+from ndlib.viz.mpl.PrevalenceComparison import DiffusionPrevalenceComparison
+from ndlib.viz.mpl.TrendComparison import DiffusionTrendComparison
+
+import ndlib.models.ModelConfig as mc
+import ndlib.models.epidemics.SIRModel as sir
+import ndlib.models.epidemics.SIModel as si
+from ndlib.viz.mpl.DiffusionTrend import DiffusionTrend
+
+
+class VizTest(unittest.TestCase):
+
+    def test_visualize(self):
+        g = nx.erdos_renyi_graph(1000, 0.1)
+        model = sir.SIRModel(g)
+        config = mc.Configuration()
+        config.add_model_parameter('beta', 0.001)
+        config.add_model_parameter('gamma', 0.01)
+        config.add_model_parameter("percentage_infected", 0.05)
+        model.set_initial_status(config)
+        iterations = model.iteration_bunch(200)
+        trends = model.build_trends(iterations)
+
+        # Visualization
+        viz = DiffusionTrend(model, trends)
+        viz.plot("diffusion.pdf")
+        os.remove("diffusion.pdf")
+
+    def test_visualize_prevalence(self):
+        g = nx.erdos_renyi_graph(1000, 0.1)
+        model = sir.SIRModel(g)
+        config = mc.Configuration()
+        config.add_model_parameter('beta', 0.001)
+        config.add_model_parameter('gamma', 0.01)
+        config.add_model_parameter("percentage_infected", 0.05)
+        model.set_initial_status(config)
+        iterations = model.iteration_bunch(200)
+        trends = model.build_trends(iterations)
+
+        # Visualization
+        viz = DiffusionPrevalence(model, trends)
+        viz.plot("prev.pdf")
+        os.remove("prev.pdf")
+
+    def test_prevalence_comparison(self):
+
+        # Network topology
+        g = nx.erdos_renyi_graph(1000, 0.1)
+
+        # Model selection
+        model = sir.SIRModel(g)
+
+        # Model Configuration
+        cfg = mc.Configuration()
+        cfg.add_model_parameter('beta', 0.001)
+        cfg.add_model_parameter('gamma', 0.02)
+        cfg.add_model_parameter("percentage_infected", 0.01)
+        model.set_initial_status(cfg)
+
+        iterations = model.iteration_bunch(200)
+        trends = model.build_trends(iterations)
+
+        model1 = si.SIModel(g)
+        cfg = mc.Configuration()
+        cfg.add_model_parameter('beta', 0.001)
+        cfg.add_model_parameter("percentage_infected", 0.01)
+        model1.set_initial_status(cfg)
+
+        iterations = model1.iteration_bunch(200)
+        trends1 = model1.build_trends(iterations)
+
+        viz = DiffusionPrevalenceComparison([model, model1], [trends, trends1])
+        viz.plot("prev_comparison.pdf")
+        os.remove("prev_comparison.pdf")
+
+    def test_trend_comparison(self):
+
+        # Network topology
+        g = nx.erdos_renyi_graph(1000, 0.1)
+
+        # Model selection
+        model = sir.SIRModel(g)
+
+        # Model Configuration
+        cfg = mc.Configuration()
+        cfg.add_model_parameter('beta', 0.001)
+        cfg.add_model_parameter('gamma', 0.02)
+        cfg.add_model_parameter("percentage_infected", 0.01)
+        model.set_initial_status(cfg)
+
+        iterations = model.iteration_bunch(200)
+        trends = model.build_trends(iterations)
+
+        model1 = si.SIModel(g)
+        cfg = mc.Configuration()
+        cfg.add_model_parameter('beta', 0.001)
+        cfg.add_model_parameter("percentage_infected", 0.01)
+        model1.set_initial_status(cfg)
+
+        iterations = model1.iteration_bunch(200)
+        trends1 = model1.build_trends(iterations)
+
+        viz = DiffusionTrendComparison([model, model1], [trends, trends1])
+
+        viz.plot("trend_comparison.pdf")
+        os.remove("trend_comparison.pdf")
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ ndlib/test/test_parallel.py
@@ -0,0 +1,49 @@
+import unittest
+import networkx as nx
+import ndlib.models.ModelConfig as mc
+import ndlib.models.epidemics.SIRModel as sir
+from ndlib.utils import multi_runs
+
+
+class ParallelTest(unittest.TestCase):
+
+    def test_multi(self):
+
+        # Network topology
+        g = nx.erdos_renyi_graph(1000, 0.1)
+
+        # Model selection
+        model1 = sir.SIRModel(g)
+
+        # Model Configuration
+        config = mc.Configuration()
+        config.add_model_parameter('beta', 0.001)
+        config.add_model_parameter('gamma', 0.01)
+        config.add_model_parameter("percentage_infected", 0.05)
+        model1.set_initial_status(config)
+
+        # Simulation multiple execution
+        trends = multi_runs(model1, execution_number=10, iteration_number=100, infection_sets=None, nprocesses=4)
+        self.assertIsNotNone(trends)
+
+    def test_multi_initial_set(self):
+        # Network topology
+        g = nx.erdos_renyi_graph(1000, 0.1)
+
+        # Model selection
+        model1 = sir.SIRModel(g)
+
+        # Model Configuration
+        config = mc.Configuration()
+        config.add_model_parameter('beta', 0.001)
+        config.add_model_parameter('gamma', 0.01)
+        model1.set_initial_status(config)
+
+        # Simulation multiple execution
+        infection_sets = [(1, 2, 3, 4, 5), (3, 23, 22, 54, 2), (98, 2, 12, 26, 3), (4, 6, 9)]
+        trends = multi_runs(model1, execution_number=4, iteration_number=100, infection_sets=infection_sets,
+                            nprocesses=4)
+        self.assertIsNotNone(trends)
+
+if __name__ == '__main__':
+    unittest.main()
