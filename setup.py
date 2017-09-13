8ffeb502a8ba598e2e1c3e701612ad3531a32c8c
Giulio Rossetti <giulio.rossetti@gmail.com>
2017-09-13 14:33:47 +0200

    Setup updated

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ __email__ = "giulio.rossetti@gmail.com"
 #    long_description = f.read()
 
 setup(name='ndlib',
-      version='2.1.2',
+      version='3.0.0',
       license='GNU General Public License v3 or later (GPLv3+)',
       description='Network Diffusion Library',
       url='https://github.com/GiulioRossetti/ndlib',
