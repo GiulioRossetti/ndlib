8a6c125c556ee9cf7df710573d833a970fc9b7f1
Giulio Rossetti <giulio.rossetti@gmail.com>
2017-12-07 15:06:21 +0100

    3.1.0 Cholera

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ __email__ = "giulio.rossetti@gmail.com"
 #    long_description = f.read()
 
 setup(name='ndlib',
-      version='3.0.1',
+      version='3.1.0',
       license='GNU General Public License v3 or later (GPLv3+)',
       description='Network Diffusion Library',
       url='https://github.com/GiulioRossetti/ndlib',
