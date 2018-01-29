9d32debd37d31596b4c446ae3938199c221fb5ab
Giulio Rossetti <giulio.rossetti@gmail.com>
2018-01-29 09:20:11 +0100

    :bug: bigfix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ __email__ = "giulio.rossetti@gmail.com"
 
 
 setup(name='ndlib',
-      version='4.0.0',
+      version='4.0.1',
       license='BSD-Clause-2',
       description='Network Diffusion Library',
       url='https://github.com/GiulioRossetti/ndlib',
