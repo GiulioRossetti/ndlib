5f4daed13185d3fe95782094973327df2f4ef63a
giuliorossetti <giulio.rossetti@gmail.com>
2019-03-29 11:05:26 +0100

    :new: Ising Fluctuation Model

--- ndlib/models/epidemics/IsingFluctuationModel.py
+++ ndlib/models/epidemics/IsingFluctuationModel.py
@@ -82,11 +82,12 @@ class IFModel(DiffusionModel):
         if u == self.params['model']['N1']:
             dif /= self.params['model']['T1']
             ds_bath /= self.params['model']['T1']
+            self.ds_bath.append((self.actual_iteration + 1, ds_bath, 'T1'))
         else:
             dif /= self.params['model']['T2']
             ds_bath /= self.params['model']['T2']
+            self.ds_bath.append((self.actual_iteration + 1, ds_bath, 'T2'))
 
-        self.ds_bath.append((self.actual_iteration+1, ds_bath))
 
         if s_u_inv < s_u:
             return True
