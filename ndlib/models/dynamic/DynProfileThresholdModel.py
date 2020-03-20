6b9f5f6cddb2a58f7db7b5867f2d6e2eff792b2e
Giulio Rossetti <giulio.rossetti@gmail.com>
2020-03-20 14:34:48 +0100

    :arrow_up: setting random state

--- ndlib/models/dynamic/DynProfileThresholdModel.py
+++ ndlib/models/dynamic/DynProfileThresholdModel.py
@@ -17,13 +17,13 @@ class DynProfileThresholdModel(DynamicDiffusionModel):
         :param threshold: The node threshold. As default a value of 0.1 is assumed for all nodes.
     """
 
-    def __init__(self, graph):
+    def __init__(self, graph, seed=None):
         """
              Model Constructor
 
              :param graph: A networkx graph object
          """
-        super(self.__class__, self).__init__(graph)
+        super(self.__class__, self).__init__(graph, seed)
         self.available_statuses = {
             "Susceptible": 0,
             "Infected": 1,
