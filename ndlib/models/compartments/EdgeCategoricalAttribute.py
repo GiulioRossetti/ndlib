cef60cee1ecb2a88b3e70bbeae466e2708247790
neserende <anneserende@gmail.com>
2024-03-25 12:41:58 +0300

    Fixed the error about edge_attr dict in cases where edge is defined as (large num, small num) instead of (small num, large num)

--- ndlib/models/compartments/EdgeCategoricalAttribute.py
+++ ndlib/models/compartments/EdgeCategoricalAttribute.py
@@ -23,24 +23,47 @@ class EdgeCategoricalAttribute(Compartiment):
 
     def execute(self, node, graph, status, status_map, *args, **kwargs):
         neighbors = list(graph.neighbors(node))
+        isDiGraph = False #true if the current graph is directed
+        
         if isinstance(graph, nx.DiGraph):
             neighbors = list(graph.predecessors(node))
+            isDiGraph = False
 
         edge_attr = graph.get_edge_attributes(self.attribute)
 
         if self.trigger is not None:
-            triggered = [
-                v
-                for v in neighbors
-                if status[v] == status_map[self.trigger]
-                and edge_attr[(min([node, v]), max([node, v]))] == self.attribute_value
-            ]
+            if(isDiGraph):
+                triggered = [
+                    v
+                    for v in neighbors
+                    if status[v] == status_map[self.trigger]
+                    and edge_attr[(min([node, v]), max([node, v]))] == self.attribute_value
+                ]
+            else:
+                triggered = []
+                for v in neighbors:
+                    if((node, v) in edge_attr):
+                        check = edge_attr[(node, v)]
+                    else: #then graph.has_edge(v, node) is True because v is given as neighbor of node
+                        check = edge_attr[(v, node)]
+                    if status[v] == status_map[self.trigger] and check == self.attribute_value:
+                        triggered.append(v)
         else:
-            triggered = [
-                v
-                for v in neighbors
-                if edge_attr[(min([node, v]), max([node, v]))] == self.attribute_value
-            ]
+            if(isDiGraph):
+                triggered = [
+                    v
+                    for v in neighbors
+                    if edge_attr[(min([node, v]), max([node, v]))] == self.attribute_value
+                ]
+            else:
+                triggered = []
+                for v in neighbors:
+                    if((node, v) in edge_attr):
+                        check = edge_attr[(node, v)]
+                    else:
+                        check = edge_attr[(v, node)]
+                    if check == self.attribute_value:
+                        triggered.append(v)
 
         for _ in triggered:
             p = np.random.random_sample()
